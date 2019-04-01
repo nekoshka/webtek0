b035d1814daea557ac1f6f8c4c4eb01a8e5aba99
IntNum <37394615+IntNum@users.noreply.github.com>
2019-04-01 15:15:06 +0800

    Add files via upload

---
+++ webtek0-master/webtek0-master/exercise/Script.js
@@ -0,0 +1,4 @@
+$.getJSON("https://wger.de/api/v2/exercise/?id=587&language=2&category=8&format=json&Authorization_Token=624a61d08a2a02fa9d4b3cc6f6e3a76b09ef16b9", function(data) {
+	console.log(data); 
+		/*optional stuff to do after success */
+});
