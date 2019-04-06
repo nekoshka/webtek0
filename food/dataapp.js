f7c68dd0ca40586ac25f9060551da1e4ae1bd4ae
Adam <2165086@slu.edu.ph>
2019-04-07 01:20:49 +0700

    renewed food folder version added some style fix some bugs and added the press enter only functionality

--- food/dataapp.js
+++ food/dataapp.js
@@ -1,22 +1,50 @@
-document.getElementById('gettext').addEventListener('click',gettext);
+var button = document.getElementById('gettext');
 var input = document.getElementById('findThis');
+var parentUl = document.getElementById('list');
+input.addEventListener("keyup" ,function(event){
+    if(event.keyCode === 13){
+        event.preventDefault();
+        document.getElementById('gettext').click();
+    }
+});
 
 function gettext() {
     fetch('https://api.edamam.com/api/food-database/parser?ingr='+input.value+'&app_id=aabfeda2&app_key=4876ff911a79e2817beadea9ef4c9828')
     .then((res) => res.json())
     .then((data) => {
         try {
-            getValue('Energy (Kcal) ' + data.parsed[0].food.nutrients.ENERC_KCAL);
-            getValue('Fat ' + data.parsed[0].food.nutrients.FAT);
-            getValue('Protien ' + data.hints[0].food.nutrients.PROCNT);
-            getValue('Category ' + data.parsed[0].food.category);
+            try {
+                removeElem();
+                getValue('Food: ' + data.text);
+                getValue('Energy (Kcal): ' + data.parsed[0].food.nutrients.ENERC_KCAL);
+                getValue('Fat: ' + data.parsed[0].food.nutrients.FAT);
+                getValue('Protien: ' + data.hints[0].food.nutrients.PROCNT);
+                getValue('Category: ' + data.parsed[0].food.category);
+            }catch {
+                getValue('Food: ' + data.text);
+                getValue('Energy (Kcal): ' + data.parsed[0].food.nutrients.ENERC_KCAL);
+                getValue('Fat: ' + data.parsed[0].food.nutrients.FAT);
+                getValue('Protien: ' + data.hints[0].food.nutrients.PROCNT);
+                getValue('Category: ' + data.parsed[0].food.category);
+            }
         }catch {
             try {
-                getValue('Energy (Kcal) ' + data.hints[0].food.nutrients.ENERC_KCAL);
-                getValue('Fat ' + data.hints[0].food.nutrients.FAT);
-                getValue('Protien ' + data.hints[0].food.nutrients.PROCNT);
-                getValue('Category ' + data.hints[0].food.category);
-                getValue('Contents ' + data.hints[0].food.foodContentsLabel);
+                try {
+                    removeElem();
+                    getValue('Food: ' + data.text);
+                    getValue('Energy (Kcal): ' + data.hints[0].food.nutrients.ENERC_KCAL);
+                    getValue('Fat: ' + data.hints[0].food.nutrients.FAT);
+                    getValue('Protien: ' + data.hints[0].food.nutrients.PROCNT);
+                    getValue('Category: ' + data.hints[0].food.category);
+                    getValue('Contents: ' + data.hints[0].food.foodContentsLabel);
+                }catch {
+                    getValue('Food: ' + data.text);
+                    getValue('Energy (Kcal): ' + data.hints[0].food.nutrients.ENERC_KCAL);
+                    getValue('Fat: ' + data.hints[0].food.nutrients.FAT);
+                    getValue('Protien: ' + data.hints[0].food.nutrients.PROCNT);
+                    getValue('Category: ' + data.hints[0].food.category);
+                    getValue('Contents: ' + data.hints[0].food.foodContentsLabel);
+                }
             }catch {
                 getValue("What is That Food?")
             }
@@ -27,5 +55,11 @@ function getValue(type) {
     var li = document.createElement('li');
     var value = document.createTextNode(type);
     li.appendChild(value);
-    document.getElementById('list').appendChild(li);   
+    parentUl.appendChild(li);
+}
+
+function removeElem() {
+    for(var i = 0 ; i <= 5 ; i++) {
+        parentUl.removeChild(parentUl.firstChild);
+    }
 }
