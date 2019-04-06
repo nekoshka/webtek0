f5904bc463e04c686295258bf0b9ccffb3ee42ec
Adam <2165086@slu.edu.ph>
2019-04-06 13:28:00 +0700

    Added the functionality of search on food

--- food/dataapp.js
+++ food/dataapp.js
@@ -1,8 +1,31 @@
-//var food = document.getElementById("food");
-function callFood () {
-    loadJSON('https://api.edamam.com/api/food-database/parser?ingr=red%20apple&app_id=aabfeda2&app_key=4876ff911a79e2817beadea9ef4c9828',gotData,jsonp);
+document.getElementById('gettext').addEventListener('click',gettext);
+var input = document.getElementById('findThis');
 
+function gettext() {
+    fetch('https://api.edamam.com/api/food-database/parser?ingr='+input.value+'&app_id=aabfeda2&app_key=4876ff911a79e2817beadea9ef4c9828')
+    .then((res) => res.json())
+    .then((data) => {
+        try {
+            getValue('Energy (Kcal) ' + data.parsed[0].food.nutrients.ENERC_KCAL);
+            getValue('Fat ' + data.parsed[0].food.nutrients.FAT);
+            getValue('Protien ' + data.hints[0].food.nutrients.PROCNT);
+            getValue('Category ' + data.parsed[0].food.category);
+        }catch {
+            try {
+                getValue('Energy (Kcal) ' + data.hints[0].food.nutrients.ENERC_KCAL);
+                getValue('Fat ' + data.hints[0].food.nutrients.FAT);
+                getValue('Protien ' + data.hints[0].food.nutrients.PROCNT);
+                getValue('Category ' + data.hints[0].food.category);
+                getValue('Contents ' + data.hints[0].food.foodContentsLabel);
+            }catch {
+                getValue("What is That Food?")
+            }
+        }
+    });
 }
-function gotData(data) {
-    println(data    )
+function getValue(type) {
+    var li = document.createElement('li');
+    var value = document.createTextNode(type);
+    li.appendChild(value);
+    document.getElementById('list').appendChild(li);   
 }
